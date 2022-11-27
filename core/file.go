a9ffec2627a482a1ca336dd5e19bdd968f318666
hinha <martinuz.dawan9@gmail.com>
2022-11-27 10:04:11 +0700

    refactor core

--- core/file.go
+++ core/file.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 
 	"github.com/hinha/watchgo/config"
+	"github.com/hinha/watchgo/utils"
 )
 
 func NewFileReader(builder Builder) *File {
@@ -18,11 +19,19 @@ type File struct {
 }
 
 func (i *File) Open(lPath string, subPath []string) error {
-	folder := i.builder.createFolder(subPath)
-	fi, _ := os.Stat(lPath)
 
-	size := ByteSize(fi.Size())
-	maxSize := ByteSize(config.FileSystemCfg.MaxFileSize) * MB
+	folder := i.builder.createFolder(subPath)
+	if folder == "" {
+		return fmt.Errorf("error creating folder")
+	}
+
+	fi, err := os.Stat(lPath)
+	if err != nil {
+		return err
+	}
+
+	size := utils.ByteSize(fi.Size())
+	maxSize := utils.ByteSize(config.FileSystemCfg.MaxFileSize) * utils.MB
 	if size >= maxSize {
 		return fmt.Errorf("size limits on the file %s of maximum, %s", fi.Name(), maxSize.String())
 	}
